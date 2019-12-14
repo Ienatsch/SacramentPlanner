2f790cf9e1d59506b9f04c931ca3dd33ab4c7b94
Dalan Ienatsch <dienatsch93@gmail.com>
2019-12-14 01:43:01 -0500

    Project stand up

---
+++ SacramentPlanner/Models/Planner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetingPlanner.Models
+{
+    public class Planner
+    {
+        public int SacaramentID { get; set; }
+        public string WardBranch { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime SabbathDay { get; set;}
+        public string ConductingLeader { get; set;}
+        public string OpenHymn { get; set; }
+        public string OpenPrayer { get; set; }
+        public string SacramentHymn { get; set;}
+        public string ClosingHymn { get; set;}
+        public string ClosingPrayer { get; set;}
+
+
+
+
+    }
+}
---
+++ SacramentPlanner/Models/Speaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetingPlanner.Models
+{
+    public class Speaker
+    {
+        public int SpeakerID { get; set; }
+        public int SacramentID { get; set; }
+
+        public string NameSpeaker { get; set; }
+        public string SpeakerTopic { get; set; }
+    }
+}
