530f4d75e95946cd73de30d37f2c734f807bf692
Mena-Osorio-Christian <christianmena03@gmail.com>
2019-12-15 00:38:57 -0700

    Validation to models added

--- SacramentPlanner/Models/Planner.cs
+++ SacramentPlanner/Models/Planner.cs
@@ -9,18 +9,31 @@ namespace MeetingPlanner.Models
     public class Planner
     {
         public int SacaramentID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string WardBranch { get; set; }
         
-        [DataType(DataType.Date)]
+        [Display(Name= "Sabbath Day"), DataType(DataType.Date)]
         public DateTime SabbathDay { get; set;}
+
+        [Display(Name = "Conducting")]
+        [StringLength(60, MinimumLength = 3)]
         public string ConductingLeader { get; set;}
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string OpenHymn { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string OpenPrayer { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string SacramentHymn { get; set;}
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string ClosingHymn { get; set;}
+
+        [StringLength(60, MinimumLength = 3)]
         public string ClosingPrayer { get; set;}
-
-
 
 
     }
