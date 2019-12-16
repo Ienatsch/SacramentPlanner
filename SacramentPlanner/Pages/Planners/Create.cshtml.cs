1e8b411185ca2617265cb24aca4d74d3ddfdcf88
Dalan Ienatsch <dienatsch93@gmail.com>
2019-12-15 21:01:13 -0500

    Added scaffolding for Planner class

---
+++ SacramentPlanner/Pages/Planners/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MeetingPlanner.Models;
+using SacramentPlanner.Data;
+
+namespace SacramentPlanner
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SacramentPlanner.Data.SacramentPlannerContext _context;
+
+        public CreateModel(SacramentPlanner.Data.SacramentPlannerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Planner Planner { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Planner.Add(Planner);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ SacramentPlanner/Pages/Planners/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MeetingPlanner.Models;
+using SacramentPlanner.Data;
+
+namespace SacramentPlanner
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SacramentPlanner.Data.SacramentPlannerContext _context;
+
+        public IndexModel(SacramentPlanner.Data.SacramentPlannerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Planner> Planner { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Planner = await _context.Planner.ToListAsync();
+        }
+    }
+}
