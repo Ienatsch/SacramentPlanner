3e454cdb9ea8c7c48f07c25933c059c672f43eef
Dalan Ienatsch <dienatsch93@gmail.com>
2019-12-16 21:43:45 -0500

    Removed uneeded Speaker class fields

--- SacramentPlanner/Models/Speaker.cs
+++ SacramentPlanner/Models/Speaker.cs
@@ -8,9 +8,6 @@ namespace MeetingPlanner.Models
 {
     public class Speaker
     {
-        public int SpeakerID { get; set; }
-        public int SacramentID { get; set; }
-
         public string NameSpeaker { get; set; }
         public string SpeakerSubject { get; set; }
     } 
